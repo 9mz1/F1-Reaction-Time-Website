c522e9b4e845c5c1c816ecde116d3eab9d2851c5
9mz1 <muzyyyyyyy@gmail.com>
2025-06-22 13:40:47 +0400

    Made the text change

--- app.js
+++ app.js
@@ -21,24 +21,32 @@ function startTimer() {
     lights.forEach((light) => {
         light.classList.add('active');
     })
+    info.textContent = 'CLICK NOW'
     startTime = performance.now();
     timerStarted = true;
     waitingForGreen = false
 }
 
 function calculateReaction() {
+    contentContainer.removeEventListener('click', main);
     endTime = performance.now();
     reactionTime = (endTime - startTime).toFixed(1);
 
-    if (bestReactionTime == 0 || reactionTime < bestReactionTime) {
+    if (parseFloat(bestReactionTime) == 0 || parseFloat(reactionTime) < parseFloat(bestReactionTime)) {
         bestReactionTime = reactionTime;
+        info.textContent = 'New Record';
     }
 
-    console.log(bestReactionTime);
+    timeText.textContent = `${reactionTime}ms`;
+    bestTimeText.textContent = `Best Time - ${bestReactionTime}ms`;
+    console.log(typeof bestReactionTime);
+    console.log(typeof reactionTime);
     console.log(`reaction time: ${reactionTime}`);
+    contentContainer.addEventListener('click', reset);
 }
 
 function reset() {
+    contentContainer.removeEventListener('click', reset);
     setTimeout(() => {
         lights.forEach((light) => {
             light.classList.remove('active');
@@ -48,24 +56,27 @@ function reset() {
         startTime = 0;
         endTime = 0;
         reactionTime = 0;
-    }, 1500)
+
+        info.textContent = 'Click To Start';
+        contentContainer.addEventListener('click', main);
+    }, 100)
 }
 
 function main() {
     if (!waitingForGreen && !timerStarted) {
+        info.textContent = 'Get Ready';
         waitingForGreen = true;
         timeOut = setTimeout(() => {
             startTimer();
             console.log('started');
         }, startDelay());
-        reset();
     } else if (waitingForGreen && !timerStarted) {
+        info.textContent = 'Jump Start';
         console.log('jumpStart');
         clearTimeout(timeOut);
         reset();
     } else if (timerStarted) {
         calculateReaction();
-        reset();
     }
 }
 
